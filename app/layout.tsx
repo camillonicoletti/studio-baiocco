b0b99e38458b20380a8d02cd54377d6bb1dc0d6a
Camillo Nicoletti <camillo.nicoletti@mail.polimi.it>
2026-03-09 07:21:03 +0100

    add sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,9 @@ import Script from "next/script";
 import type { ReactNode } from "react";
 
 export const metadata = {
-  title: "Studio Baiocco",
-  description: "Studio Baiocco - Consulente del Lavoro",
-  viewport: "width=device-width, initial-scale=1.0",
+  title: 'Studio Romana Baiocco - Consulente del Lavoro a Matera',
+  description: 'Studio Baiocco, Consulente del Lavoro a Matera. Elaborazione Paghe, Amministrazione del Personale e Consulenza Aziendale Professionale.',
+  keywords: 'studio baiocco, studio romana baiocco, romana baiocco, consulente del lavoro, studio consulente del lavoro, consulente del lavoro matera, studio baiocco matera, studio romana baiocco matera, consulente del lavoro studio baiocco matera, studio baiocco consulente del lavoro matera, matera studio romana baiocco, matera studi consulenti del lavoro, consulenza del lavoro matera, consulenza lavoro matera, commercialista lavoro matera, studio paghe matera, elaborazione paghe matera, buste paga matera, gestione paghe matera, amministrazione personale matera, gestione personale matera, assunzioni matera, contratti lavoro matera, studio professionale matera, consulente lavoro basilicata, studio baiocco basilicata, paghe basilicata, consulenza aziendale matera, risorse umane matera, gestione dipendenti matera, TFR matera, contributi INPS matera, cedolini paga matera, apertura partita iva matera, lavoro dipendente matera, normativa lavoro matera',
 };
 
 type RootLayoutProps = {
